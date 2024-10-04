3ab893fb7bd73eecd1fbe75cc7c6e3ecb8d00d98
siyangjiangart <jiangsiyang7@gmail.com>
2024-10-04 05:01:05 +0200

    firs commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
+import {Model} from "./Model.jsx";
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
-  });
+
 
   return (
     <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
+      <Model />
       <ambientLight />
     </>
   );
